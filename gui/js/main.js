b8ef3607f8f9e0fd308200a2ecb1497f7cd301ec
Fredrik Frodlund <frippz@me.com>
2014-10-14 15:04:42 +0200

    Delete jQuery and rename common.js to main.js

---
+++ gui/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * main.js
+ *
+ * Only runs in modern browsers via feature detection
+ */
+
+if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
+  
+  
+  
+}
