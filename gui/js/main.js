3d6b3f3a2568c64e8d75f8cdd7c73138d17df618
Fredrik Frodlund <frippz@me.com>
2014-11-20 14:04:04 +0100

    Add class "js" to html element

--- gui/js/main.js
+++ gui/js/main.js
@@ -5,7 +5,8 @@
  */
 
 if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
-  
-  
-  
+
+  // Add class "js" to html element
+  document.querySelector('html').classList.add('js');
+
 }
